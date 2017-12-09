595258c3c2ee9d8b262639d7af3b3c4769bd489b
Daniel Sakuma <dsakuma@gmail.com>
2017-12-09 19:59:26 -0200

    Test 0,1,2,3 ok

--- ep3/closureTyped.rkt
+++ ep3/closureTyped.rkt
@@ -7,7 +7,6 @@
   [idC   (s : symbol)]
   [plusC (l : ExprC) (r : ExprC)]
   [multC (l : ExprC) (r : ExprC)]
-  ;[lamC  (arg : symbol) (body : ExprC)]
   [appC  (fun : ExprC) (arg : ExprC)]
   [ifC   (c : ExprC) (y : ExprC) (n : ExprC)]
   [seqC  (e1 : ExprC) (e2 : ExprC)]
@@ -15,7 +14,7 @@
   [letC  (name : symbol) (arg : ExprC) (body : ExprC)]
   [classC (parent : symbol) (ins-var : symbol) (m1 : ExprC) (m2 : ExprC)]
   [methodC (name : symbol) (arg : symbol) (body : ExprC)]
-  [newC (class : symbol) (arg : ExprC)]
+  [newC (class : symbol) (arg : number)]
   [sendC (obj : ExprC) (method-name : symbol) (param : ExprC)]
   )
 
@@ -24,7 +23,6 @@
 (define-type ExprS
   [numS    (n : number)]
   [idS     (s : symbol)]
-  ;[lamS    (arg : symbol) (body : ExprS)]
   [appS    (fun : ExprS) (arg : ExprS)]
   [plusS   (l : ExprS) (r : ExprS)]
   [bminusS (l : ExprS) (r : ExprS)]
@@ -36,7 +34,7 @@
   [letS    (name : symbol) (arg : ExprS) (body : ExprS)]
   [classS  (parent : symbol) (ins-var : symbol) (m1 : ExprS) (m2 : ExprS)]
   [methodS (name : symbol) (arg : symbol) (body : ExprS)]
-  [newS    (class : symbol) (arg : ExprS)]
+  [newS    (class : symbol) (arg : number)]
   [sendS   (obj : ExprS) (method-name : symbol) (param : ExprS)]
   )
 
@@ -46,7 +44,6 @@
   (type-case ExprS as
     [numS    (n)        (numC n)]
     [idS     (s)        (idC s)]
-    ;[lamS    (a b)      (lamC a (desugar b))]
     [appS    (fun arg)  (appC (desugar fun) (desugar arg))]
     [plusS   (l r)      (plusC (desugar l) (desugar r))]
     [multS   (l r)      (multC (desugar l) (desugar r))]
@@ -58,7 +55,7 @@
     [letS    (n a b)    (letC n (desugar a) (desugar b))]
     [classS  (parent ins-var m1 m2) (classC parent ins-var (desugar m1) (desugar m2))]
     [methodS (name arg body) (methodC name arg (desugar body))]
-    [newS    (class arg) (newC class (desugar arg))]
+    [newS    (class arg) (newC class arg)]
     [sendS   (obj method-name param) (sendC (desugar obj) method-name (desugar param))]
     ))
 
@@ -68,7 +65,7 @@
   [numV  (n : number)]
   [methodV (name : symbol) (arg : symbol) (body : ExprC)]
   [classV (parent : symbol) (ins-var : symbol) (m1 : Value) (m2 : Value)]
-  [objectV (class : symbol) (parent : Value)]
+  [objectV (class : symbol) (parent : Value) (obj-env : (listof Binding))]
   )
 
 
@@ -89,12 +86,16 @@
 
 ; Find the name of a variable
 (define (lookup [for : symbol] [env : Env]) : (boxof Value)
+  (begin
+    ;(display "symbol: ")
+    ;(display for)
+    ;(display "\n")
        (cond
             [(empty? env) (error 'lookup (string-append (symbol->string for) " was not found"))] ; variable is undefined
             [else (cond
                   [(symbol=? for (bind-name (first env)))   ; found it!
                                  (bind-val (first env))]
-                  [else (lookup for (rest env))])]))        ; check in the rest
+                  [else (lookup for (rest env))])])))        ; check in the rest
 
 
 ; Auxiliary operators
@@ -116,50 +117,44 @@
 (define (create-object-set-ins-vars class arg env) ; Wallet/0/env
   
   (if (equal? class 'Object)
-      (begin ;(display "Encontou classe Object\n")
-             (objectV class (objectV 'DummyClass (numV 1) )))
+      (objectV class (objectV 'DummyClass (numV 1) env) env)
 
-      (begin ;(display "Criando objeto\n")
+      (begin
+             (display "variavel de instancia: ") 
+             (display  (classV-ins-var  (unbox (lookup class env))))
+             (display "\n")
              (let* [(classValue (unbox (lookup class env)))
-                    (parentClassId (classV-parent classValue))]
-                               (objectV class (create-object-set-ins-vars parentClassId 0 env))))))
+                    (parentClassId (classV-parent classValue))
+                    (extended-env (extend-env (bind (classV-ins-var classValue) (box (numV arg))) env))]
+                               (objectV class
+                                        (create-object-set-ins-vars parentClassId 0 env)
+                                        extended-env)))))
 
 (define (find-method obj method-name env)
-  (begin (display "Environment: ")
-         (display env)
-         (display "\n\n")
+  (begin ;(display "Environment: ")
+         ;(display env)
+         ;(display "\n\n")
 
-         (display "Object ")
-         (display obj)
-         (display "\n\n")
+         ;(display "Object ")
+         ;(display obj)
+         ;(display "\n\n")
 
-         (display "Parent: ")
-         (display (objectV-parent obj))
-         (display "\n\n")
+         ;(display "Parent: ")
+         ;(display (objectV-parent obj))
+         ;(display "\n\n")
 
          
          (let* ([class (objectV-class obj)]
+                [classValue (unbox (lookup class env))]
                 [parentClass (objectV-class (objectV-parent obj))])
-           (begin
-             
-             (display "AAAAAAAAAAAA")
-             
-             (if (equal? parentClass 'DummyClass)
-                 (error 'find-method (string-append "Class does not respond to the method " (symbol->string method-name)))
-                 (error 'find-method (string-append "Class does not respond to the method " (symbol->string method-name))))))))
+           (if (equal? parentClass 'DummyClass)
+               (error 'find-method (string-append "Class does not respond to the method " (symbol->string method-name)))
 
-
-;         (let* ([class (objectV-class obj)]
-;                [parentClass (objectV-class (objectV-parent obj))])
-;           
-;           (if (equal? parentClass 'DummyClass)
-;               (error 'find-method (string-append "Class does not respond to the method " (symbol->string method-name)))
-;
-;               (if (equal? (methodV-name (classV-m1 class)) method-name)
-;                   (classV-m1 class)
-;                   (if (equal? (methodV-name (classV-m2 class)) method-name)
-;                       (classV-m2 class)
-;                       (find-method (objectV-parent obj) method-name env)))))))
+               (if (equal? (methodV-name (classV-m1 classValue)) method-name)
+                   (classV-m1 classValue)
+                   (if (equal? (methodV-name (classV-m2 classValue)) method-name)
+                       (classV-m2 classValue)
+                       (find-method (objectV-parent obj) method-name env)))))))
                         
            
            
@@ -172,10 +167,7 @@
 
     ; IDs are retrieved from the Env and unboxed
     [idC (n) (unbox (lookup n env))]
-
-    ; Lambdas evaluate to closures, which save the environment
-    ;[lamC (a b) (methodV a b env)]
-
+    
     ; Application of function
     [appC (f a) (numV 99999)]
               
@@ -219,8 +211,15 @@
                           ;(display result)
                            result))]
 
-    [sendC (obj method-name param) (let* ([objectValue (interp obj env)])
-                                          (find-method objectValue method-name env))]
+    [sendC (obj method-name param) (let* ([objectValue (interp obj env)]
+                                          [method (find-method objectValue method-name env)]
+                                          [method-body (methodV-body method)]
+                                          [method-arg (methodV-arg method)])
+                                          (interp method-body
+                                                  (extend-env (bind 'self (box objectValue))
+                                                              (extend-env
+                                                                 (bind method-arg (box (interp param env)))
+                                                                 (objectV-obj-env objectValue)))))]
     ))
 
 
@@ -246,28 +245,18 @@
                       (parse (third sl)))]
          [(method) (methodS (s-exp->symbol (second sl)) (s-exp->symbol (third sl))  (parse (fourth sl)))]
          [(class) (classS (s-exp->symbol (second sl)) (s-exp->symbol (third sl)) (parse (fourth sl)) (parse (fourth (rest sl))))]
-         [(new) (newS (s-exp->symbol (second sl)) (parse (third sl)))]
+         [(new) (newS (s-exp->symbol (second sl)) (s-exp->number (third sl)))]
          [(send) (sendS (parse (second sl)) (s-exp->symbol (third sl)) (parse (fourth sl)))]
          [else (error 'parse "invalid list input")]))]
     [else (error 'parse "invalid input")]))
 
 
 ; Facilitator
-(define (interpS [s : s-expression]) (interp (desugar (parse s)) mt-env))
+(define (interpS [s : s-expression]) (interp (desugar (parse s)) env))
 
 
 ; Examples
-;(interpS '(+ 10 (call (lambda x (+ (:= x (* x 2)) x)) 8)))
-
-;(interpS '(call (lambda x (seq (:= x 1) x)) 2))
-
 ;(interpS '(let ([x 10]) x))
-
-; My tests
-
-;(interpS '(class ClassA var1 (method m1 x (+ x var1)) (method m2 x (+ x var1))))
-
-
 
 
 ;(interpS '(new ObjectA 0)) ; It works because class object already exists in global env
@@ -294,3 +283,69 @@
               (send wallet debit 3)))))
   (numV 7))
 
+
+
+
+; Test #2: User-defined class inheriting from Object, with method that delegates
+;          to another via self.
+(test
+  (interpS
+    '(let ([Wallet
+             (class Object money
+                    (method credit amount (:= money (+ money amount)))
+                    (method debit amount (send self credit (~ amount))) )])
+       (let ([wallet (new Wallet 0)])
+         (seq (send wallet credit 10)
+              (send wallet debit 3)))))
+  (numV 7))
+
+; Test #3: User-defined class inheriting from Object, calling a method that does
+;          not exist.
+(test/exn
+  (interpS
+    '(let ([Wallet
+             (class Object money
+                    (method credit amount (:= money (+ money amount)))
+                    (method debit amount (send self credit (~ amount))) )])
+       (let ([wallet (new Wallet 0)])
+         (seq (send wallet credit 10)
+              (send wallet deduction 3))))) ; <-- Method does not exist!
+  "Class does not respond to the method deduction")
+
+; Test #4: User-define class inheriting from another user-defined class,
+;          with method from child overriding the parent's implementation,
+;          method from parent delegating to overriden method, and method
+;          from child accessing attribute of the parent.
+(test
+  (interpS '(let ([Wallet
+                    (class Object money
+                           (method credit amount (:= money (+ money amount)))
+                           (method debit amount (send self credit (~ amount))) )])
+              (let ([WalletWithTaxes
+                      (class Wallet tax
+                             (method credit amount (:= money (- (+ money amount) tax)))
+                             (method total dummy money) )])
+                (let ([wallet (new WalletWithTaxes 1)])
+                  (seq (send wallet credit 10)
+                       (seq (send wallet debit 3)
+                            (send wallet total 0))
+                       )))))
+  (numV 5))
+
+;; Test #5: User-define class inheriting from another user-defined class,
+;;          calling a method that does not exist.
+;(test/exn
+;  (interpS '(let ([Wallet
+;                    (class Object money
+;                           (method credit amount (:= money (+ money amount)))
+;                           (method debit amount (send self credit (~ amount))) )])
+;              (let ([WalletWithTaxes
+;                      (class Wallet tax
+;                             (method credit amount (:= money (- (+ money amount) tax)))
+;                             (method total dummy money) )])
+;                (let ([wallet (new WalletWithTaxes 1)])
+;                  (seq (send wallet credit 10)
+;                       (seq (send wallet debit 3)
+;                            (send wallet amount 0)) ; <-- Method does not exist!
+;                       )))))
+;  "Class does not respond to the method amount")
